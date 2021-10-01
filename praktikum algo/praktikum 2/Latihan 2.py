4a35e56eb40b8034829cff0acca782a2bc5e8891
Yuda Hadi Prasetyo <91053096+yudahadi79@users.noreply.github.com>
2021-10-01 17:19:09 +0700

    Latihan2.py
    Ini adalah hasil dari praktikum 2 latihan 2

--- praktikum algo/praktikum 2/Latihan 2.py
+++ praktikum algo/praktikum 2/Latihan 2.py
@@ -4,22 +4,28 @@ NIM : 065002100004
 Jurusan Sistem Informasi
 Algoritma dan pemrograman 
 Praktikum 2 
-30 september 2021
+1 oktober 2021
 """
 #Latihan Kedua
+#Haversine Formula
 import math as m
 print("Haloooo")
 print("ini yuda")
 print('selamat datang diprogram perhitungan jarak antara dua titik di permukaan bumi')
 
-t1=float(input("Masukkan nilai latitude 1 : "))
-t2=float(input('masukkan nilai latitude 2 : '))
-g1=float(input("Masukkan nilai longitude 1 : "))
-g2=float(input('masukkan nilai longitude 2 : '))
+lat1=float(input("Masukkan nilai latitude kota pertama : "))
+lon1=float(input("Masukkan nilai longitude kota pertama : "))
+lat2=float(input('masukkan nilai latitude kota kedua : '))
+lon2=float(input('masukkan nilai longitude kota kedua : '))
 
-#Haversine Formula
-R = 6.371
-a = m.pow(m.sin(t2-t1/2),2)+m.cos(t1)*m.cos(t2)*m.pow(m.sin(g2-g1/2),2)
+R = 6371.01
+t1 = m.radians(lat1)
+t2 = m.radians(lat2)
+g1 = m.radians(lon1)
+g2 = m.radians(lon2)
+t = t2-t1
+g = g2-g1
+a = m.pow(m.sin(t/2),2)+m.cos(t1)*m.cos(t2)*m.pow(m.sin(g/2),2)
 c = 2*m.atan2(m.sqrt(a),m.sqrt(1-a))
 d = R*c
 
